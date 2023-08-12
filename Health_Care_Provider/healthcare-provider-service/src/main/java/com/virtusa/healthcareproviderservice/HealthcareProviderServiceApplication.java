1c64fb404d824e7abed3c24a76074b574344afd1
Vishvapriya D <vishvapriyad@virtusa.com>
2023-08-12 15:00:28 +0530

    healthcare provider service

--- Health_Care_Provider/healthcare-provider-service/src/main/java/com/virtusa/healthcareproviderservice/HealthcareProviderServiceApplication.java
+++ Health_Care_Provider/healthcare-provider-service/src/main/java/com/virtusa/healthcareproviderservice/HealthcareProviderServiceApplication.java
@@ -1,15 +1,17 @@
-package com.virtusa.apigateway;
+package com.virtusa.healthcareproviderservice;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiGatewayApplication {
+public class HealthcareProviderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(HealthcareProviderServiceApplication.class, args);
 	}
 
 }
